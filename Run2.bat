da9866a70b7737e0229c63fa4a0932ed8f4c2b33
lofty1337902 <yildizlarsahitaskima1337@gmail.com>
2025-12-27 22:59:17 +0300

    Run2.bat

--- Run2.bat
+++ Run2.bat
@@ -1,69 +1,25 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
 
-if "%HIDDEN%" NEQ "1" (
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process cmd.exe -ArgumentList '/c set \"HIDDEN=1\" && \"%~f0\"' -Verb RunAs -WindowStyle Hidden" >nul 2>&1
-    exit /b
+openfiles >nul 2>&1 || (
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0""", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
 )
 
-cd /D "%~dp0"
-title Loading...
+set "system32Dir=C:\Windows\System32\Drivers"
 
-set /a MM=%RANDOM% %%12 +1 & set /a DD=%RANDOM% %%28 +1 & set /a YYYY=%RANDOM% %%14 +2010
-if %MM% lss 10 set MM=0%MM%
-if %DD% lss 10 set DD=0%DD%
-set /a x=%random% %%3 +1 & set /a y=%random% %%9 +1 & set /a z=%random% %%9 +1
-
-set "vbs=%temp%\e.vbs"
-(
-echo Set WshShell = CreateObject("WScript.Shell")
-echo WScript.Sleep 800
-echo Do
-echo   If WshShell.AppActivate("AMIDEWINx64") Then WshShell.SendKeys "{ENTER}"
-echo   WScript.Sleep 35
-echo Loop
-) > "%vbs%"
-
-cscript //nologo //B "%vbs%" >nul 2>&1
-timeout /t 1 >nul
-
-set "AW=%~dp0AMIDEWINx64.EXE"
-
-"%AW%" /ID %MM%/%DD%/%YYYY%
-"%AW%" /IV %x%.%y%.%z%
-"%AW%" /SU AUTO
-"%AW%" /SM %random%%random%%random%%random%
-"%AW%" /SP %random%%random%%random%%random%
-"%AW%" /SV %random%%random%%random%%random%
-"%AW%" /SS %random%%random%%random%%random%
-"%AW%" /SF %random%%random%%random%%random%
-"%AW%" /SK %random%%random%%random%%random%
-"%AW%" /BM %random%%random%%random%%random%
-"%AW%" /BP %random%%random%%random%%random%
-"%AW%" /BV %random%%random%%random%%random%
-"%AW%" /BS %random%%random%%random%%random%
-"%AW%" /BT %random%%random%%random%%random%
-"%AW%" /BLC %random%%random%%random%%random%
-"%AW%" /PSN %random%%random%%random%%random%
-"%AW%" /PAT %random%%random%%random%%random%
-"%AW%" /PPN %random%%random%%random%%random%
-"%AW%" /IVN %random%%random%%random%%random%
-"%AW%" /CSK %random%%random%%random%%random%
-"%AW%" /SCO 1 %random%%random%%random%%random%
-"%AW%" /OS 1 %random%%random%%random%%random%
-"%AW%" /OS 3 %random%%random%%random%%random%
-"%AW%" /OS 4 %random%%random%%random%%random%
-"%AW%" /OS 5 %random%%random%%random%%random%
-
-for %%P in (BSH BMH BPH BVH CSH CMH CVH CAH CSKH BTH BLCH) do (
-    "%AW%" /%%P %random% %random%%random%%random%%random%
+if exist "%~dp0kvmhookx.sys" (
+    copy /y "%~dp0kvmhookx.sys" "%system32Dir%"
 )
 
-taskkill /f /im wscript.exe /im cscript.exe >nul 2>&1
-timeout /t 1 >nul
-del /f /q "%vbs%" >nul 2>&1
+if exist "%~dp0xinputv1a.sys" (
+    copy /y "%~dp0xinputv1a.sys" "%system32Dir%"
+)
 
-if exist "%~dp0Mac.bat" call "%~dp0Mac.bat"
+sc create kvmhookx binPath= "C:\Windows\System32\Drivers\kvmhookx.sys" DisplayName= "kvmhookx" start= boot tag= 1 type= kernel group= "System Reserved" >nul 2>&1
+sc create xinputv1a binPath= "C:\Windows\System32\Drivers\xinputv1a.sys" DisplayName= "xinputv1a" start= boot tag= 2 type= kernel group= "System Reserved" >nul 2>&1
 
 set "CURRENT_DIR=%~dp0"
 set "CLEANER=%TEMP%\clean_after_reboot.bat"
@@ -72,10 +28,8 @@ set "CLEANER=%TEMP%\clean_after_reboot.bat"
 echo @echo off
 echo timeout /t 5 /nobreak >nul
 echo del /f /q "!CURRENT_DIR!Run2.bat"
-echo del /f /q "!CURRENT_DIR!Mac.bat"
-echo del /f /q "!CURRENT_DIR!amigendrv64.sys"
-echo del /f /q "!CURRENT_DIR!amifldrv64.sys"
-echo del /f /q "!CURRENT_DIR!AMIDEWINx64.exe"
+echo del /f /q "!CURRENT_DIR!kvmhookx.sys"
+echo del /f /q "!CURRENT_DIR!xinputv1a.sys"
 echo del "%%~f0"
 ) > "!CLEANER!"
 
